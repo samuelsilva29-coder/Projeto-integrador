fb84cba03a7b224387ad6c8697bef0abca20eb66
Samuel Santos Costa Silva <202511900001@ifto.local>
2026-06-02 12:08:47 -0300

    Subindo arquivos POO

--- POO/carro.py
+++ POO/carro.py
@@ -21,25 +21,47 @@ class Carro:
         self.ano = ano
         self.velocidade = velocidade
 
+    # "aumento" será o valor para aumentar a velocidade.
     def acelerar(self, aumento):
+        # self.velocidade = self.velocidade + aumento
         self.velocidade = self.velocidade + aumento
         self.velocidade += aumento
 
         print(f"O carro acelerou para {self.velocidade} km/h")
+
+    # Metodo frear 
+    def frear(self, reducao):
+        # self.velocidade = self.velocidade - reducao
+        self.velocidade -= reducao
+
+        print(f"O carro reduziu para {self.velocidade} km/h")
+    
+    # Método para exibir informações 
+    def exibir_info(self):
+        print("=== INFORMAÇÕES DO CARRO ===")
+
+        # exibe os atributos do objeto
+        print(f"Marca: {self.marca}")
+        print(f"Modelo: {self.modelo}")
+        print(f"ano: {self.ano}")
+        print(f"velocidade atual: {self.velocidade}")
     
 # "carro1" é uma variável que recebe um objeto
 carro1 = Carro("Chevrolet", "S10", 2013)
 
-# Exibir informações do carro
-print(f"Marca: {carro1.marca}")
-print(f"Modelo: {carro1.modelo}")
-print(f"ano: {carro1.ano}")
-print(f"ano: {carro1.velocidade}")
 
-carro1.acelerar(1000000000000000)
+# chamando os metodos
+# O valor 50 será enviado para o parâmetro "aumento"
+carro1.acelerar(50)
 
-carro2 = Carro("Hyundai", "Creta", 2024)
+# O valor 20 será enviado para o parâmetro "redução"
+carro1.frear(20)
 
-print(f"Marca: {carro2.marca}")
-print(f"Modelo: {carro2.modelo}")
-print(f"Ano: {carro2.ano}")
+# exibindo as informações do carro
+carro1.exibir_info()
+
+#  carro2 = Carro("Hyundai", "Creta", 2024)
+
+# print(f"Marca: {carro2.marca}")
+# print(f"Modelo: {carro2.modelo}")+
+# print(f"Ano: {carro2.ano}")
