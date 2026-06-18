9a3d946a5c718ead0b90afc12d7bf763199b611c
Samuel Santos Costa Silva <202511900001@ifto.local>
2026-06-18 11:49:34 -0300

    DDL E DML prontos

--- Banco de dados/DDL/Projeto  catalogo_filmes.sql
+++ Banco de dados/DDL/Projeto  catalogo_filmes.sql
@@ -1,16 +1,21 @@
 CREATE DATABASE catalogo_filmes;
 USE catalogo_filmes;
 
+-- criação da tabela usuarios (independente)
 CREATE TABLE usuarios (
     id_usuario      INT AUTO_INCREMENT PRIMARY KEY,
     nome            VARCHAR(100) NOT NULL,
     email           VARCHAR(100) NOT NULL UNIQUE,
     senha           VARCHAR(255) NOT NULL
 );
+
+-- criação da tabela generos (também independente)
 CREATE TABLE generos (
     id_genero       INT AUTO_INCREMENT PRIMARY KEY,
     nome_genero     VARCHAR(100) NOT NULL
 );
+
+-- criação da tabela filmes (Tem dependência de usuarios e generos, mas aceita NULL)
 CREATE TABLE filmes (
     id_filme        INT AUTO_INCREMENT PRIMARY KEY,
     titulo          VARCHAR(200) NOT NULL,
@@ -20,6 +25,7 @@ CREATE TABLE filmes (
     id_usuario      INT NULL,
     id_genero       INT NULL,
 
+    --relacionamentos (FKs)
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
     FOREIGN KEY (id_genero) REFERENCES generos(id_genero)
 );
