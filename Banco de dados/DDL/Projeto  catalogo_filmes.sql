8eb8a856485d67fd6fc2bc9d25e2ef5e860f20b1
Samuel Santos <samuel.silva29@estudante.ifto.edu.br>
2026-06-19 15:26:50 -0300

    Versão atualizada do DDL e DML - 19/06

--- Banco de dados/DDL/Projeto  catalogo_filmes.sql
+++ Banco de dados/DDL/Projeto  catalogo_filmes.sql
@@ -1,7 +1,10 @@
+-- ========================================================
+-- CRIAÇÃO DO BANCO DE DADOS catalogo_filmes
+-- ========================================================
 CREATE DATABASE catalogo_filmes;
 USE catalogo_filmes;
 
--- criação da tabela usuarios (independente)
+-- criação da tabela usuarios (tabela base)
 CREATE TABLE usuarios (
     id_usuario      INT AUTO_INCREMENT PRIMARY KEY,
     nome            VARCHAR(100) NOT NULL,
@@ -9,23 +12,27 @@ CREATE TABLE usuarios (
     senha           VARCHAR(255) NOT NULL
 );
 
--- criação da tabela generos (também independente)
+-- criação da tabela generos (tabela dependente de usuarios)
 CREATE TABLE generos (
     id_genero       INT AUTO_INCREMENT PRIMARY KEY,
-    nome_genero     VARCHAR(100) NOT NULL
+    nome_genero     VARCHAR(100) NOT NULL,
+    id_usuario      INT NOT NULL,
+
+    -- relacionamento: todo gênero deve ser cadastrado por um usuario existente
+    FOREIGN KEY id_usuario REFERENCES usuarios(id_usuario)
 );
 
--- criação da tabela filmes (Tem dependência de usuarios e generos, mas aceita NULL)
+-- criação da tabela filmes (Tem dependência de usuarios e generos)
 CREATE TABLE filmes (
     id_filme        INT AUTO_INCREMENT PRIMARY KEY,
     titulo          VARCHAR(200) NOT NULL,
     ano_lancamento  YEAR(4) NOT NULL,
-    avaliacao       (2,1),
+    avaliacao       DECIMAL (2,1),
     ator_principal  VARCHAR(100) NULL,
-    id_usuario      INT NULL,
-    id_genero       INT NULL,
+    id_usuario      INT NOT NULL,
+    id_genero       INT NOT NULL,
 
-    --relacionamentos (FKs)
+    --relacionamentos: todo filme filme tem dependencia de um usuario e genero
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
     FOREIGN KEY (id_genero) REFERENCES generos(id_genero)
 );
--- Banco de dados/DML/Projeto catalogo_filmes.sql
+++ Banco de dados/DML/Projeto catalogo_filmes.sql
@@ -1,5 +1,5 @@
--- INSERINDO OS DADOS DA TABELA USUÁRIOS
-
+--  1. INSERT (CRIANDO OS USUARIOS DA TABELA usuarios)
+===================================================
 INSERT INTO usuarios (id_usuario, nome, email, senha) VALUES
 (1, 'Samuel Santos', 'samuel7@gmail.com', '1010'),
 (2, 'Arthur', 'arthur@gmail.com', '2020'),
@@ -7,55 +7,81 @@ INSERT INTO usuarios (id_usuario, nome, email, senha) VALUES
 (4, 'Luiz', 'luiz11@gmail.com', '4040'),
 (5, 'Maria', 'maria1@gmail.com', '5050');
 
--- generos criados: Ficção científica, Ação, Infatil, Comédia e Terror
-INSERT INTO filmes (titulo, ano_lancamento, avaliacao, ator_principal, id_genero)
+-- CRIANDO OS GÊNEROS DA TABELA generos
+-- =======================================================
+INSERT INTO generos (id_genero, nome_genero, id_usuario) VALUES
+(1, 'Ficção Científica', 1), -- Ficção Científica (id_genero = 1)
+(2, 'Ação', 2), -- Ação (id_genero = 2)
+(3, 'Infantil', 3), -- Infantil (id_genero = 3)
+(4, 'Comédia', 4), -- Comédia (id_genero = 4)
+(5, 'Terror', 5);  -- terror (id_genero = 5)
+
+-- CRIANDO DOS FILMES DA TABELA filmes
+-- =======================================================
+INSERT INTO filmes (id_genero, titulo, ano_lancamento, avaliacao, ator_principal, id_usuario)
 VALUES
 -- filmes com id_genero = 1
-('Interstelar', 2014, 7.8, 'Matthew McConaughey',1),
-('Guerra dos Mundos', 2005, 7.7, 'Tom Cruise', 1),
+(1,'Interstelar', 2014, 7.8, 'Matthew McConaughey', 1),
+(1,'Guerra dos Mundos', 2005, 7.7, 'Tom Cruise', 2),
 
 -- filmes com id_genero = 2
-
-('John Wick', 2014, 8.4, 'Keanu Reeves', 2),
-('Thor', 2011, 7.1, 'Chris Hemsworth', 2),
+(2,'John Wick', 2014, 8.4, 'Keanu Reeves', 3),
+(2,'Thor', 2011, 7.1, 'Chris Hemsworth', 3),
 
 -- filmes com id_genero = 3
-('Procurando Nemo', 2003, 7.1, 'Nemo', 3),
-('Toy Story 1', 1995, 6.9, 'Xerife Woody', 3),
+(3,'Procurando Nemo', 2003, 7.1, 'Nemo', 1),
+(3,'Toy Story 1', 1995, 6.9, 'Xerife Woody', 4),
 
 -- filmes com id_genero = 4
-('Esposa de Mentirinha', 2011, 7.6, 'Adam Sandler', 4),
-('O Mentiroso', 1997, 7.3, 'Jim Carrey', 4),
+(4,'Esposa de Mentirinha', 2011, 7.6, 'Adam Sandler', 4),
+(4,'O Mentiroso', 1997, 7.3, 'Jim Carrey', 5),
 
 -- filmes com id_genero = 5
-('A Freira', 2018, 6.5, 'Bonnie Aarons', 5),
-('A Bruxa', 2015, 7.0, 'Anya Taylor-Joy', 5);
+(5,'A Freira', 2018, 6.5, 'Bonnie Aarons', 2),
+(5,'A Bruxa', 2015, 7.0, 'Anya Taylor-Joy', 5);
 
--- consultando os usuários criados
+-- 2. SELECT 
+-- ================================================================
+-- consultando os usuarios criados
 SELECT * FROM usuarios;
 
---consultando o usuário 1 
-SELECT * FROM usuarios WHERE nome = 'Samuel Santos';
+--consultando o 1º usuário  
+SELECT * FROM usuarios WHERE id_usuario = 1;
 		
 -- consultando os gêneros criados
 SELECT * FROM generos;
 
--- consultando um gênero especifico
+-- consultando o 3º genero
 SELECT * FROM generos WHERE id_genero = 3;
 
 -- consultando todos os filmes criados
 SELECT * FROM filmes;
 
--- consultando um filme especifico
+-- consultando o filme Toy Story 1
 SELECT * FROM filmes WHERE titulo = 'Toy Story 1';
 
--- utilizando o UPDATE para atualizar o filme John wick
-UPDATE filmes SET titulo = 'John Wick 2' WHERE ator_principal = 'Keanu Reeves';
+-- 3. UPDATE 
+-- ================================================================
+-- Alterando o filme Esposa de mentirinha para Zohan
+UPDATE filmes SET titulo = 'Zohan: O Agente Bom de Corte' WHERE 'Esposa de Mentirinha';
 
--- Atualizando um usuário da tabela filmes
-UPDATE usuários
-SET nome = 'José' WHERE id_usuario = 4;
+-- Atualizando o nome do 4º usuario
+UPDATE usuarios SET nome = 'José' WHERE id_usuario = 4;
 
--- utilizando o DELETE para deletar um usuário
-DELETE FROM usuarios
-WHERE id_usuario = 2;
+-- atualizando o genero terror
+UPDATE generos SET nome_genero = 'Suspense' WHERE id_genero = 5;
+
+-- 4. DELETE
+-- =============================================================
+-- deletando o filme Guerra dos Mundos
+DELETE FROM filmes 
+WHERE id_filme= 2;
+
+-- deletando o filme Interstelar
+DELETE FROM filmes
+WHERE id_filme = 1;
+
+-- deletando o genero Ficção cientifica
+DELETE FROM generos 
+WHERE id_genero = 1;
+
